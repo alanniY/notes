d33f01d47cdfc615d0f70bd0d0b023d4d3dcae2c
yangyundong <1223913450@qq.com>
2016-08-12 10:22:06 +0800

    temp

--- js/notes.js
+++ js/notes.js
@@ -6,12 +6,10 @@ $(function(){
     $('.tab-item').removeClass('active');
     $('#' + id).addClass('active');
   })
-  $('.item p').hover(
-    function(){
-      $('.item').parents('.tab-item').find('code').hide(1000);
-      $(this).parents('.item').find('code:first-child').show('fast', function(){
-        $(this).next().show('fast', arguments.callee);
-      }),
-    function(){}
+  $('.item p').click(function(){
+    $('.item').parents('.tab-item').find('code').hide(1000);
+    $(this).parents('.item').find('code:first-child').show('fast', function(){
+      $(this).next().show('fast', arguments.callee);
+    });
   });
 });
